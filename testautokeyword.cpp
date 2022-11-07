4895c14091ef8d6c376bdb467de0443e50533ca7
quangpersie <quangnttwin@gmail.com>
2022-11-07 20:57:49 +0700

    taone

---
+++ testautokeyword.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+// 	auto x = 5;
+//	string s = "Concak nha tao";
+//	double s = 4.5;
+	bool s = true;
+ 	cout << typeid(s) << endl;
+	return 0;
+}
+
